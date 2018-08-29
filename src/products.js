79ea74e73e746690c493e278a63c6921dbf72633
Raichel John <raicheljohn28@gmail.com>
2018-08-29 17:50:40 -0400

    updates

---
+++ src/products.js
@@ -0,0 +1,122 @@
+const teamList = [
+    {   id: 1,
+        team: "Broccoli",
+        price: 15,
+        image: "img/broccoli.jpg",
+        quantity: 0
+    },
+    {
+        id: 2,
+        team: "Beetroot",
+        price: 20,
+        image: "img/beetroot.jpg",
+        quantity: 0
+    },
+    {
+        id: 3,
+        team: "Beans",
+        price: 23,
+        image: "img/beans.jpg",
+        quantity: 0
+    },
+    {
+        id: 4,
+        team: "Carrots",
+        price: 27,
+        image: "img/carrots.jpg",
+        quantity: 0
+    },
+    {
+        id: 5,
+        team: "Corn",
+        price: 18,
+        image: "img/corn.jpg",
+        quantity: 0
+    },
+    {
+        id: 6,
+        team: "Potato",
+        price: 22,
+        image: "img/potato.jpg",
+        quantity: 0
+    },
+    {
+        id: 7,
+        team: "Tomato",
+        price: 26,
+        image: "img/tomato.jpg",
+        quantity: 0
+    },
+    {
+        id: 8,
+        team: "Pomegranate",
+        price: 42,
+        image: "img/pomegranate.jpg",
+        quantity: 0
+    },
+    {
+        id: 9,
+        team: "Mango",
+        price: 25,
+        image: "img/mango.jpg",
+        quantity: 0
+    },
+    {
+        id: 10,
+        team: "Cucumber",
+        price: 24,
+        image: "img/cucumber.jpg",
+        quantity: 0
+    },
+    {
+        id: 11,
+        team: "Strawberry",
+        price: 40,
+        image: "img/strawberry.jpg",
+        quantity: 0
+    },
+    {
+        id: 12,
+        team: "Musk Melon",
+        price: 36,
+        image: "img/musk-melon.jpg",
+        quantity: 0
+    },
+    {
+        id: 13,
+        team: "Raspberry",
+        price: 37,
+        image: "img/raspberry.jpg",
+        quantity: 0
+    },
+    {
+        id: 14,
+        team: "Cashews",
+        price: 50,
+        image: "img/cashews.jpg",
+        quantity: 0
+    },
+    {
+        id: 15,
+        team: "Almonds",
+        price: 60,
+        image: "img/almonds.jpg",
+        quantity: 0
+    },
+    {
+        id: 16,
+        team: "Pista",
+        price: 37,
+        image: "img/pista.jpg",
+        quantity: 0
+    },
+    {
+        id: 17,
+        team: "Mixed Nuts",
+        price: 69,
+        image: "img/nuts-mixture.jpg",
+        quantity: 0
+    }
+]
+
+export {teamList};
